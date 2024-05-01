868eed1b7d64f9700b18f29e2da7fc0a0505f85e
Afonina Tatiana <99973342+tafonina@users.noreply.github.com>
2024-05-01 14:21:43 +0100

    Dev 562 gas snapshots (#4)
    * restructured contracts to compare clones with beacon for gas, added new param paiSymbol to EOfeed which can be different to description
    
    * tests for eofeed deployed with clone and beacon
    
    * reorder storage vars
    
    * add gap to base adapter

--- src/adapters/factories/EOFeedFactoryClone.sol
+++ src/adapters/factories/EOFeedFactoryClone.sol
@@ -8,10 +8,23 @@ import { EOFeedFactoryBase } from "./EOFeedFactoryBase.sol";
 abstract contract EOFeedFactoryClone is Initializable, EOFeedFactoryBase {
     address private _feedImplementation;
 
+    /**
+     * @dev Returns the address of the feed implementation.
+     */
+    function getFeedImplementation() external view returns (address) {
+        return _feedImplementation;
+    }
+
+    /**
+     * @dev Initializes the factory with the feed implementation.
+     */
     function __EOFeedFactory_init(address impl, address) internal override onlyInitializing {
         _feedImplementation = impl;
     }
 
+    /**
+     * @dev Deploys a new feed instance via Clones library.
+     */
     function _deployEOFeed() internal override returns (address) {
         return Clones.clone(_feedImplementation);
     }
