d4c99e630594a6aa44bfe22ed66141383b2860c4
Sergei Liakh <sergei@eoracle.io>
2024-05-23 12:05:18 +0100

    DEV-698: multi checkpoint test

--- test/integration/Integration.t.sol
+++ test/integration/Integration.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.25;
 
 import { IEOFeedManager } from "../../src/interfaces/IEOFeedManager.sol";
-import { IEOFeedVerifier } from "../../src/interfaces/IEOFeedVerifier.sol";
 import { IntegrationBaseTests } from "./IntegrationBase.t.sol";
 import { EOJsonUtils } from "../..//script/utils/EOJsonUtils.sol";
 import { InvalidProof } from "../../src/interfaces/Errors.sol";
@@ -65,7 +64,7 @@ contract IntegrationMultipleLeavesSingleCheckpointTests is IntegrationBaseTests 
     /**
      * @notice should not allow to update feed with data not related to merkle root
      */
-    function test_RevertWhen_updatePriceFeed_UnsignedData(bytes memory data) public {
+    function testFuzz_RevertWhen_updatePriceFeed_UnsignedData(bytes memory data) public {
         vm.assume(keccak256(input[0].unhashedLeaf) != keccak256(data));
         input[0].unhashedLeaf = data;
         vm.startPrank(_publisher);
@@ -87,63 +86,30 @@ contract IntegrationMultipleLeavesSingleCheckpointTests is IntegrationBaseTests 
             assertEq(_feedRegistryAdapter.getFeedById(_feedIds[i]).latestAnswer(), int256(_rates[i]));
         }
     }
+}
 
-    function _generatePayload(bytes[] memory _feedsData) internal override {
-        require(_feedsData.length > 0, "FEEDSDATA_EMPTY");
-        uint256 len = 5 + _feedsData.length;
-        string[] memory cmd = new string[](len);
-        cmd[0] = "npx";
-        cmd[1] = "ts-node";
-        cmd[2] = "test/utils/ts/generateMsgProofRates.ts";
-        cmd[3] = vm.toString(abi.encode(DOMAIN));
-        cmd[4] = vm.toString(abi.encode(VALIDATOR_SET_SIZE));
-        for (uint256 i = 0; i < _feedsData.length; i++) {
-            cmd[5 + i] = vm.toString(_feedsData[i]);
-        }
+// solhint-disable max-states-count
+contract IntegrationMultipleCheckpointsTests is IntegrationBaseTests {
+    function test_updatePriceFeed_SameFeedsMultipleCheckpoints() public {
+        vm.prank(_publisher);
+        _feedManager.updatePriceFeed(input[0], checkpoints[0], signatures[0], bitmaps[0]);
+        IEOFeedManager.PriceFeed memory feedAdapter = _feedManager.getLatestPriceFeed(_feedIds[0]);
+        assertEq(feedAdapter.value, _rates[0]);
+        assertEq(_feedRegistryAdapter.getFeedById(_feedIds[0]).latestAnswer(), int256(_rates[0]));
 
-        bytes memory out = vm.ffi(cmd);
-        bytes[] memory unhashedLeaves;
-        bytes32[][] memory proves;
-        bytes32[] memory hashes;
-        bytes[] memory _bitmaps;
-        uint256[2][] memory aggMessagePoints;
+        vm.warp(block.timestamp + 1);
 
-        IEOFeedVerifier.Validator[] memory validatorSetTmp;
+        EOJsonUtils.Config memory configStructured = EOJsonUtils.getParsedConfig();
+        // create new rates
+        _seedfeedsData(configStructured, uint256(99));
+        // create new checkpoint
+        _generatePayload(feedsData);
+        _setValidatorSet(validatorSet);
 
-        (validatorSetSize, validatorSetTmp, aggMessagePoints, hashes, _bitmaps, unhashedLeaves, proves,) = abi.decode(
-            out,
-            (uint256, IEOFeedVerifier.Validator[], uint256[2][], bytes32[], bytes[], bytes[], bytes32[][], bytes32[][])
-        );
-
-        for (uint256 i = 0; i < validatorSetSize; i++) {
-            validatorSet.push(validatorSetTmp[i]);
-        }
-
-        for (uint256 i = 0; i < _feedsData.length; i++) {
-            input.push(IEOFeedVerifier.LeafInput({ unhashedLeaf: unhashedLeaves[i], leafIndex: i, proof: proves[i] }));
-
-            // solhint-disable-next-line func-named-parameters
-        }
-        signatures.push(aggMessagePoints[0]);
-        checkpoints.push(
-            IEOFeedVerifier.Checkpoint({
-                blockNumber: 1,
-                epoch: 1,
-                eventRoot: hashes[0],
-                blockHash: hashes[1],
-                blockRound: 0
-            })
-        );
-
-        bitmaps.push(_bitmaps[0]);
-    }
-
-    function _seedfeedsData(EOJsonUtils.Config memory configStructured) internal override {
-        for (uint256 i = 0; i < configStructured.supportedFeedIds.length; i++) {
-            _feedIds.push(uint16(configStructured.supportedFeedIds[i]));
-            _rates.push(100 + configStructured.supportedFeedIds[i]);
-            _timestamps.push(9_999_999_999);
-            feedsData.push(abi.encode(_feedIds[i], _rates[i], _timestamps[i]));
-        }
+        vm.prank(_publisher);
+        _feedManager.updatePriceFeed(input[0], checkpoints[0], signatures[0], bitmaps[0]);
+        feedAdapter = _feedManager.getLatestPriceFeed(_feedIds[0]);
+        assertEq(feedAdapter.value, _rates[0]);
+        assertEq(_feedRegistryAdapter.getFeedById(_feedIds[0]).latestAnswer(), int256(_rates[0]));
     }
 }
