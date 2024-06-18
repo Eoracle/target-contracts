67b0e04ca35f11451b6b95349d83864833ae3efd
Tetiana Afonina <tetiana@eoracle.io>
2024-06-18 17:30:34 +0100

    upgraded FeedVerifier with new setters, upgrade scripts and commands

---
+++ script/upgrade/UpgradeFeedVerifierUnsafe.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.25;
+
+import { Script } from "forge-std/Script.sol";
+import { stdJson } from "forge-std/Script.sol";
+import { EOJsonUtils } from "script/utils/EOJsonUtils.sol";
+import { UpgradeableProxyUtils } from "../utils/UpgradeableProxyUtils.sol";
+
+contract UpgradeFeedVerifierUnsafe is Script {
+    using stdJson for string;
+
+    function run() external {
+        string memory config = EOJsonUtils.initOutputConfig();
+        address feedVerifierProxyAddress = config.readAddress(".feedVerifier");
+
+        vm.startBroadcast();
+        UpgradeableProxyUtils.upgradeProxy(feedVerifierProxyAddress, "EOFeedVerifier.sol", "");
+        vm.stopBroadcast();
+
+        string memory outputConfigJson = EOJsonUtils.OUTPUT_CONFIG.serialize(
+            "feedVerifierImplementation", UpgradeableProxyUtils.getImplementationAddress(feedVerifierProxyAddress)
+        );
+        EOJsonUtils.writeConfig(outputConfigJson);
+    }
+}
